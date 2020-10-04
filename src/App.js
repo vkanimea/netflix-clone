b666cb5e8c105b9aef2868cdd03db01d39398b5d
Vincent Kanimea <vincentk@spc.int>
2020-10-04 20:40:15 +1200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-     <h1> Hey Clever Programmer! Lets Build</h1>
+      <h1> Netflix Clone </h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" />
+      <Row title="Action Movies" />
+      <Row title="Comedy Movies" />
+      <Row title="Horror Movies" />
+      <Row title="Romance Movies" />
+      <Row title="Documentaries" />
     </div>
   );
 }
---
+++ src/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+/** base url to make request to the movie database */
+const instance = axios.create({
+    baseURL: "/3",
+});
+
+export default instance;
